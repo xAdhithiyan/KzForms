a66000b3dbee4ad8bc76411e24979a852906cb5b
adhithiyan <adhithiyan.work@gmail.com>
2023-09-02 21:33:39 +0530

    add layout and styling for the image section

--- src/components/FormLayout.jsx
+++ src/components/FormLayout.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 
 
 function FormLayout(props){
-    console.log(props.label.value)
     return (
         <>
             <div className="inputs">
                 <label htmlFor= {props.name}>{props.label}</label>
-                <input type={props.type} id={props.name} name={props.name} required/>
+                <input type={props.type} id={props.name} name={props.name} value={props.value} onChange={props.changeValues} required/>
             </div>
         </>
     )
 }
+
 export default FormLayout;
